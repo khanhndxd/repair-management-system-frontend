b95bf4c1ccf5bf176bc1cc36b0ed960dac444fb2
khanhndxd <khanhprox25x@gmail.com>
2023-09-27 12:39:39 +0700

    Set default route to /quan-ly

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/quan-ly',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
