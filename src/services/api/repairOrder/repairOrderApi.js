825572d543e3a2336c1cddd998fa23184b2f12df
khanhndxd <khanhprox25x@gmail.com>
2023-11-06 22:41:53 +0700

    Add tags to endpoints and add api endpoint to update repair order status

--- src/services/api/repairOrder/repairOrderApi.js
+++ src/services/api/repairOrder/repairOrderApi.js
@@ -4,9 +4,11 @@ const repairOrderApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getAllRepairOrders: build.query({
       query: () => "/RepairOrder/GetAll",
+      providesTags: (result, error, id) => [{ type: "RepairOrders", id: "List" }],
     }),
-    getRepairOrderByCustomerId: build.query({
-      query: (id) => `/RepairOrder/Customer/${id}`,
+    getRepairOrderById: build.query({
+      query: (id) => `/RepairOrder/${id}`,
+      providesTags: (result, error, id) => [{ type: "RepairOrders", id: "Single" }],
     }),
     addRepairOrder: build.mutation({
       query: (body) => ({
@@ -15,7 +17,23 @@ const repairOrderApi = baseApi.injectEndpoints({
         body,
       }),
     }),
+    updateRepairOrderStatus: build.mutation({
+      query: (body) => ({
+        url: "/RepairOrder",
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: [
+        { type: "RepairOrders", id: "Single" },
+        { type: "RepairOrders", id: "List" },
+      ],
+    }),
   }),
 });
 
-export const { useGetAllRepairOrdersQuery, useGetRepairOrderByCustomerIdQuery, useAddRepairOrderMutation } = repairOrderApi;
+export const {
+  useGetAllRepairOrdersQuery,
+  useGetRepairOrderByIdQuery,
+  useAddRepairOrderMutation,
+  useUpdateRepairOrderStatusMutation,
+} = repairOrderApi;
