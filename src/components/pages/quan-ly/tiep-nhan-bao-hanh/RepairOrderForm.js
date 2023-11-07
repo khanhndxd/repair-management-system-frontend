1222058915da908828a568138e995b1c9645626a
khanhndxd <khanhprox25x@gmail.com>
2023-11-07 22:28:22 +0700

    Make component reusable for creating and editing

--- src/components/pages/quan-ly/tiep-nhan-bao-hanh/RepairOrderForm.js
+++ src/components/pages/quan-ly/tiep-nhan-bao-hanh/RepairOrderForm.js
@@ -1,77 +1,37 @@
 "use client";
 
-import { showNotification } from "@/store/features/notificationSlice";
-import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styles from "@/styles/main.module.scss";
 import CustomerForm from "./CustomerForm";
 import RepairOrderInfoForm from "./RepairOrderInfoForm";
 import RepairList from "./RepairList";
-import { format } from "date-fns";
-import { useAddRepairOrderMutation } from "@/services/api/repairOrder/repairOrderApi";
 
-export default function RepairOrderForm() {
-  const [addRepairOrder, {isLoading}] = useAddRepairOrderMutation();
+export default function RepairOrderForm(props) {
+  const { mode, initialValues, onSubmit } = props;
   const repairOrder = useSelector((state) => state.repairOrder);
-  const dispatch = useDispatch();
-  const router = useRouter();
   const {
     control,
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm({
-    defaultValues: {
-      creator: "1",
-      receiver: null,
-      createdDate: format(new Date(), "yyyy-MM-dd"),
-      receiveDate: format(new Date(), "yyyy-MM-dd"),
-      receiveType: "",
-      repairType: null,
-      repairReason: null,
-      task: null,
-      note: "",
-    },
+    defaultValues: initialValues,
   });
-
-  const onSubmit = async (data) => {
-    console.log(data);
-    console.log(repairOrder.tasksProducts);
-    let payload = {
-      CustomerId: +data.customer.id,
-      CreatedById: data.creator,
-      RepairedById: data.receiver,
-      CreatedAt: data.createdDate,
-      ReceiveAt: data.receiveDate,
-      ReceiveType: data.receiveType,
-      TotalPrice: repairOrder.total,
-      StatusId: 1,
-      RepairTypeId: +data.repairType,
-      RepairReasonId: +data.repairReason,
-      TaskId: +data.task,
-      Note: data.note,
-      ProductId: 1,
-      ProductDescription: "test",
-    };
-    const result = await addRepairOrder(payload);
-    console.log(result)
-    dispatch(showNotification({ message: "Tạo phiếu thành công", type: "success" }));
-    // router.push("/quan-ly/chi-tiet-don/1");
-  };
 
   return (
     <form className={styles["dashboard__neworder__content"]} onSubmit={handleSubmit(onSubmit)} noValidate>
       <div className={styles["dashboard_neworder__content__actions"]}>
         <button name="submit" className={styles["button"]}>
-          Thêm mới
+          {mode === "create" ? "Thêm mới" : "Xác nhận chỉnh sửa"}
         </button>
       </div>
       <div className={styles["dashboard__neworder__content"]}>
         <div className={styles["dashboard__neworder__content__info"]}>
           <div className={styles["dashboard__neworder__content__info__box"]}>
             <h3>&#10112; Thông tin khách hàng</h3>
-            <CustomerForm control={control} errors={errors} />
+            <CustomerForm control={control} errors={errors} getValues={getValues} />
           </div>
           <div className={styles["dashboard__neworder__content__info__box"]}>
             <h3>&#10113; Thông tin đơn</h3>
