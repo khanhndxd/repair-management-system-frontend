bbd0628c836646c112e37c4127d8f494ea9c039e
khanhndxd <khanhprox25x@gmail.com>
2023-11-10 23:01:31 +0700

    Reset repairOrderSlice when visit any page

--- src/components/pages/main/SidebarLink.js
+++ src/components/pages/main/SidebarLink.js
@@ -2,13 +2,17 @@
 import Link from "next/link";
 import styles from "@/styles/main.module.scss";
 import { usePathname } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { reset } from "@/store/features/repairOrderSlice";
 
 export default function SidebarLink(props) {
   const { content, href } = props;
   const pathname = usePathname();
+  const dispatch = useDispatch();
 
   return (
     <Link
+      onClick={() => dispatch(reset())}
       className={pathname !== href ? styles["sidebar__link"] : styles["sidebar__link--active"]}
       href={href}
     >
