e18ca61ff4cf02850b31581083ac4a85e90114e4
khanhndxd <khanhprox25x@gmail.com>
2023-11-10 23:12:48 +0700

    Fix handleCreateRepairOrder function to handle adding new repair order

--- src/app/quan-ly/tiep-nhan-bao-hanh/page.js
+++ src/app/quan-ly/tiep-nhan-bao-hanh/page.js
@@ -7,36 +7,54 @@ import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 import { showNotification } from "@/store/features/notificationSlice";
 import { useAddRepairOrderMutation } from "@/services/api/repairOrder/repairOrderApi";
+import { useAddRepairProductMutation } from "@/services/api/repairProduct/repairProductApi";
+import { hideLoading, showLoading } from "@/store/features/loadingAsyncSlice";
+import { useEffect } from "react";
+import { reset } from "@/store/features/repairOrderSlice";
 
 export default function NewRepairOrder() {
   const router = useRouter();
   const dispatch = useDispatch();
   const [addRepairOrder, { isLoading }] = useAddRepairOrderMutation();
-  const repairOrder = useSelector(state => state.repairOrder);
+  const [updateRepairProduct, { loading: productLoading }] = useAddRepairProductMutation();
+  const repairOrder = useSelector((state) => state.repairOrder);
+
+  useEffect(() => {
+    if (repairOrder.isOrderFromCustomerPage === false) {
+      dispatch(reset());
+    }
+  }, []);
 
   const handleCreateRepairOrder = async (data) => {
-    console.log(data);
-    // console.log(repairOrder.tasksProducts);
-    // let payload = {
-    //   CustomerId: +data.customer.id,
-    //   CreatedById: data.creator,
-    //   RepairedById: data.receiver,
-    //   CreatedAt: data.createdDate,
-    //   ReceiveAt: data.receiveDate,
-    //   ReceiveType: data.receiveType,
-    //   TotalPrice: repairOrder.total,
-    //   StatusId: 1,
-    //   RepairTypeId: +data.repairType,
-    //   RepairReasonId: +data.repairReason,
-    //   TaskId: +data.task,
-    //   Note: data.note,
-    //   ProductId: 1,
-    //   ProductDescription: "test",
-    // };
-    // const result = await addRepairOrder(payload);
-    // console.log(result)
+    try {
+      dispatch(showLoading({ content: "Đang tạo đơn bảo hành..." }));
+      let payload = {
+        CustomerId: +data.customer.id,
+        CreatedById: data.creator,
+        RepairedById: data.receiver,
+        CreatedAt: data.createdDate,
+        ReceiveAt: data.receiveDate,
+        ReceiveType: data.receiveType,
+        TotalPrice: repairOrder.total,
+        StatusId: 1,
+        RepairTypeId: +data.repairType,
+        RepairReasonId: +data.repairReason,
+        TaskId: +data.task,
+        Note: data.note,
+      };
+      const result = await addRepairOrder(payload);
+
+      dispatch(showLoading({ content: "Đang cập nhật sản phẩm bảo hành..." }));
+      let repairProductsPayload = repairOrder.products.map((item) => {
+        return { RepairOrderId: result.data.data, purchasedProductId: item.id };
+      });
+      const repairProductResult = await updateRepairProduct(repairProductsPayload);
+      dispatch(hideLoading());
+      router.push(`/quan-ly/chi-tiet-don/${result.data.data}`);
+    } catch (err) {
+      console.log(err);
+    }
     dispatch(showNotification({ message: "Tạo phiếu thành công", type: "success" }));
-    // router.push("/quan-ly/chi-tiet-don/1");
   };
   return (
     <div className={styles["dashboard__neworder"]}>
@@ -44,7 +62,7 @@ export default function NewRepairOrder() {
       <RepairOrderForm
         mode={"create"}
         initialValues={{
-          customer: null,
+          customer: repairOrder.customer,
           creator: "1",
           receiver: null,
           createdDate: format(new Date(), "yyyy-MM-dd"),
