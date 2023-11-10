0d4c10a15cc804b1f2b8ae785a18f739a0a08850
khanhndxd <khanhprox25x@gmail.com>
2023-11-10 22:42:50 +0700

    Add getCustomerById and updateCustomer apis and using tags

--- src/services/api/customer/customerApi.js
+++ src/services/api/customer/customerApi.js
@@ -1,20 +1,40 @@
-import { baseApi } from "../baseApi"
+import { baseApi } from "../baseApi";
 
 const customerApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getAllCustomers: build.query({
-      query: () => '/Customer/GetAll',
-      providesTags: (result, error, id) => [{ type: 'Customers', id: "List" }],
+      query: () => "/Customer/GetAll",
+      providesTags: (result, error, id) => [{ type: "Customers", id: "List" }],
+    }),
+    getCustomerById: build.query({
+      query: (id) => `/Customer/${id}`,
+      providesTags: (result, error, id) => [{ type: "Customers", id: "Single" }],
     }),
     addCustomer: build.mutation({
       query: (body) => ({
         url: `/Customer`,
-        method: 'POST',
+        method: "POST",
         body,
       }),
-      invalidatesTags: [{ type: 'Customers', id: 'List' }],
-    })
+      invalidatesTags: [{ type: "Customers", id: "List" }],
+    }),
+    updateCustomer: build.mutation({
+      query: (body) => ({
+        url: "/Customer",
+        method: "PATCH",
+        body,
+      }),
+      invalidatesTags: [
+        { type: "Customers", id: "List" },
+        { type: "Customers", id: "Single" },
+      ],
+    }),
   }),
-})
+});
 
-export const { useGetAllCustomersQuery, useAddCustomerMutation } = customerApi
+export const {
+  useGetAllCustomersQuery,
+  useGetCustomerByIdQuery,
+  useAddCustomerMutation,
+  useUpdateCustomerMutation,
+} = customerApi;
