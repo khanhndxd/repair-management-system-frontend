398000303717b7e658d714181ca88a9b20cdce1c
khanhndxd <khanhprox25x@gmail.com>
2023-11-25 18:02:21 +0700

    Refactor code logic for adding and removing tasks and products

--- src/store/features/repairOrderSlice.js
+++ src/store/features/repairOrderSlice.js
@@ -1,3 +1,4 @@
+import { convertToVND } from "@/services/helper/helper";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -8,9 +9,10 @@ const initialState = {
   tasks: [],
   products: [],
   anyProduct: false,
-  total: 0,
+  total: convertToVND(0),
   isWarranted: false,
   isOrderFromCustomerPage: false,
+  newRepairProducts: [],
 };
 
 export const repairOrderSlice = createSlice({
@@ -20,36 +22,133 @@ export const repairOrderSlice = createSlice({
     addCustomer: (state, action) => {
       state.customer = action.payload;
     },
-    addRepairInfo: (state, action) => {},
     addRepairType: (state, action) => {
       state.isWarranted = action.payload.object.id === 1;
+      let alpha = state.isWarranted === true ? 0 : 1;
+
+      if (alpha === 1) {
+        state.total = state.tasks.reduce((acc, currentItem) => {
+          return acc + currentItem.price;
+        }, 0);
+        state.total = convertToVND(state.total * alpha);
+      } else {
+        state.total = convertToVND(0);
+      }
+    },
+    addNewRepairProduct: (state, action) => {
+      const isDuplicateId = state.newRepairProducts.some(
+        (product) => product.id == "SPM" + action.payload.object.id
+      );
+      if (!isDuplicateId) {
+        state.newRepairProducts.push({
+          id: "SPM" + action.payload.object.id,
+          realId: action.payload.object.id,
+          name: action.payload.object.name,
+          note: action.payload.object.note,
+          description: action.payload.object.description ? action.payload.object.description : "",
+        });
+        state.tasksProducts.push({
+          id: "SPM" + action.payload.object.id,
+          name: action.payload.object.name,
+          note: action.payload.object.note,
+          type: "new",
+        });
+      }
     },
     addProduct: (state, action) => {
-      state.products.push({
-        id: action.payload.object.id,
-        name: action.payload.object.productName,
-      });
-      state.tasksProducts.push({
-        id: action.payload.object.productSerial,
-        name: action.payload.object.productName,
-      });
-      state.anyProduct = true;
+      const isDuplicateId = state.products.some((product) => product.id == action.payload.object.id);
+      if (!isDuplicateId) {
+        state.products.push({
+          id: action.payload.object.id,
+          serial: action.payload.object.productSerial,
+          name: action.payload.object.productName,
+          description: action.payload.object.description ? action.payload.object.description : ""
+        });
+        state.tasksProducts.push({
+          id: action.payload.object.productSerial,
+          name: action.payload.object.productName,
+          type: "product",
+        });
+        state.anyProduct = true;
+      }
     },
     addTask: (state, action) => {
-      state.tasks.push({ id: action.payload.object.id, name: action.payload.object.name });
-      state.tasksProducts.push({
-        id: action.payload.object.id,
-        name: action.payload.object.name,
+      const isDuplicateId = state.tasks.some((task) => task.id === action.payload.object.id);
+      if (!isDuplicateId) {
+        state.tasks.push({
+          id: action.payload.object.id,
+          name: action.payload.object.name,
+          price: action.payload.object.price,
+          description: action.payload.object.description ? action.payload.object.description : ""
+        });
+        state.tasksProducts.push({
+          id: action.payload.object.id,
+          name: action.payload.object.name,
+          price: action.payload.object.price,
+          type: "task",
+        });
+        let alpha = state.isWarranted === true ? 0 : 1;
+
+        state.total = state.tasks.reduce((acc, currentItem) => {
+          return acc + currentItem.price;
+        }, 0);
+        state.total = convertToVND(state.total * alpha);
+      }
+    },
+    updateTaskDescription: (state, action) => {
+      let index = state.tasks.findIndex((x) => x.id === action.payload.object.id);
+      if (index !== -1) {
+        state.tasks[index].description = action.payload.object.description;
+      }
+    },
+    updateProductDescription: (state, action) => {
+      let index = state.products.findIndex((x) => x.serial == action.payload.object.id);
+      if (index !== -1) {
+        state.products[index].description = action.payload.object.description;
+      }
+    },
+    updateNewProductDescription: (state, action) => {
+      let index = state.newRepairProducts.findIndex((x) => x.id == action.payload.object.id);
+      if (index !== -1) {
+        state.newRepairProducts[index].description = action.payload.object.description;
+      }
+    },
+    removeTask: (state, action) => {
+      state.tasks = state.tasks.filter((item) => {
+        return item.id !== action.payload.id;
       });
-      state.total = action.payload.object.price;
+      state.tasksProducts = state.tasksProducts.filter((item) => {
+        return item.id !== action.payload.id;
+      });
+      let alpha = state.isWarranted === true ? 0 : 1;
+
+      state.total = state.tasks.reduce((acc, currentItem) => {
+        return acc + currentItem.price;
+      }, 0);
+      state.total = convertToVND(state.total * alpha);
     },
     removeTasksProducts: (state, action) => {
       state.tasksProducts = state.tasksProducts.filter((item) => {
+        return item.id != action.payload.id;
+      });
+      state.tasks = state.tasks.filter((item) => {
         return item.id !== action.payload.id;
+      });
+      state.products = state.products.filter((item) => {
+        return item.serial != action.payload.id;
+      });
+      state.newRepairProducts = state.newRepairProducts.filter((item) => {
+        return item.id != action.payload.id;
       });
       if (action.payload.isProduct === true) {
         state.anyProduct = false;
       }
+      let alpha = state.isWarranted === true ? 0 : 1;
+
+      state.total = state.tasks.reduce((acc, currentItem) => {
+        return acc + currentItem.price;
+      }, 0);
+      state.total = convertToVND(state.total * alpha);
     },
     isOrderFromCustomerPage: (state, action) => {
       state.isOrderFromCustomerPage = action.payload.flag;
@@ -62,10 +161,14 @@ export const repairOrderSlice = createSlice({
 
 export const {
   addCustomer,
-  addRepairInfo,
   addRepairType,
+  addNewRepairProduct,
   addProduct,
   addTask,
+  updateTaskDescription,
+  updateProductDescription,
+  updateNewProductDescription,
+  removeTask,
   removeTasksProducts,
   isOrderFromCustomerPage,
   reset,
