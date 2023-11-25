86f6b976f295bc47d4edc4dfbddc9da4e6f19a32
khanhndxd <khanhprox25x@gmail.com>
2023-11-25 17:26:53 +0700

    Add more user info to cookie

--- src/store/features/authSlice.js
+++ src/store/features/authSlice.js
@@ -1,17 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { serialize, parse } from "cookie";
-
+import jwt from "jsonwebtoken";
 // Kiểm tra có đang ở browser hay ko
 const isBrowser = typeof document !== "undefined";
 
 // lấy cookie
 const cookies = isBrowser ? parse(document.cookie || "") : {};
 const user = cookies.user || null;
+const role = cookies.role || null;
 const token = cookies.token || null;
 const refreshToken = cookies.refreshToken || null;
 
 const initialState = {
   user: user,
+  role: role,
   token: token,
   refreshToken: refreshToken,
 };
@@ -21,33 +23,48 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setCredentials: (state, action) => {
-      const { user, token, refreshToken } = action.payload;
+      const { token, refreshToken } = action.payload;
+      try {
+        // Giải mã token để lấy thông tin user
+        const decodedToken = jwt.decode(token);
+        // Lưu user và token vào state
+        state.user = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"];
+        state.role = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+        state.token = token;
+        state.refreshToken = refreshToken;
 
-      setMultipleCookies([
-        { name: "user", value: user },
-        { name: "token", value: token },
-        { name: "refreshToken", value: refreshToken },
-      ]);
-
-      
-      state.user = user;
-      state.token = token;
-      state.refreshToken = refreshToken;
+        // Lưu vào cookie
+        setMultipleCookies([
+          { name: "user", value: decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] },
+          {
+            name: "role",
+            value: decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"],
+          },
+          { name: "token", value: token },
+          { name: "refreshToken", value: refreshToken },
+        ]);
+      } catch (error) {
+        console.error("Lỗi giải mã token:", error);
+        setMultipleCookies([
+          { name: "user", value: "User" },
+          { name: "user", value: "User" },
+          { name: "token", value: token },
+          { name: "refreshToken", value: refreshToken },
+        ]);
+      }
     },
     logOut: (state, action) => {
-      
       setMultipleCookies([
         { name: "user", options: { maxAge: 0 } },
+        { name: "role", options: { maxAge: 0 } },
         { name: "token", options: { maxAge: 0 } },
         { name: "refreshToken", options: { maxAge: 0 } },
       ]);
 
-      
       state.user = null;
       state.token = null;
       state.refreshToken = null;
 
-      
       if (isBrowser) {
         window.location.href = `/dang-nhap`;
       }
