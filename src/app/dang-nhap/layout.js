f593b71d814ce0b4bfae5aed242dc7e71a557406
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 16:33:24 +0700

    Clean up components

--- src/app/dang-nhap/layout.js
+++ src/app/dang-nhap/layout.js
@@ -4,7 +4,7 @@ import styles from "@/styles/main.module.scss";
 export default function LoginLayout({ children }) {
   return (
     <div className={styles["login"]}>
-      <h1>LOGO</h1>
+      <h1>ĐATN - Bảo hành sửa chữa PC và linh kiện máy tính</h1>
       {children}
     </div>
   );
