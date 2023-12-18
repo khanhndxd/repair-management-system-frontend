0cf862070ca6079db0551f75378b09e1ab2a1d32
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 16:30:42 +0700

    Add account management page menu

--- src/app/quan-ly/layout.js
+++ src/app/quan-ly/layout.js
@@ -9,13 +9,14 @@ export default function ManagementLayout({ children }) {
       <aside className={styles["sidebar"]}>
         <div className={styles["sidebar__logo"]}>
           <Link href="/">
-            <h1>LOGO</h1>
+            <h1>ĐATN</h1>
           </Link>
         </div>
         <SidebarLink content={"Trang chủ"} href="/quan-ly" />
         <SidebarLink content={"Quản lý bảo hành/sửa chữa"} href="/quan-ly/bao-hanh-sua-chua" />
         <SidebarLink content={"Quản lý khách hàng"} href="/quan-ly/khach-hang" />
         <SidebarLink content={"Tiếp nhận bảo hành"} href="/quan-ly/tiep-nhan-bao-hanh" />
+        <SidebarLink content={"Quản lý tài khoản"} href="/quan-ly/tai-khoan" />
         <SidebarLink content={"Thống kê"} href="/quan-ly/thong-ke" />
         <SidebarAccount />
       </aside>
