b739d85f0df3f37db2a056afb01206eb28135f88
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 16:16:47 +0700

    Change page layout and logic

--- src/app/quan-ly/thong-ke/page.js
+++ src/app/quan-ly/thong-ke/page.js
@@ -1,4 +1,5 @@
-import Stats from "@/components/pages/quan-ly/thong-ke/Stats";
+import Chart from "@/components/pages/quan-ly/thong-ke/Chart";
+import TopCategories from "@/components/pages/quan-ly/thong-ke/TopCategories";
 import StatsOverall from "@/components/pages/quan-ly/thong-ke/tong-quat/StatsOverall";
 import styles from "@/styles/main.module.scss";
 
@@ -10,10 +11,21 @@ export default function StatsPage() {
         <StatsOverall />
       </div>
       <div className={styles["dashboard__stats__content"]}>
-        <Stats title={"Loại sản phẩm"} type={"repairProducts"}/>
-        <Stats title={"Công việc"} type={"repairTasks"}/>
-        <Stats title={"Loại bảo hành"} type={"repairType"}/>
+        <div className={styles["dashboard__stats__content__chart"]} style={{ gridColumn: "1 / 6", gridRow: "1 / 2" }}>
+          <h3>Loại sửa chữa</h3>
+          <Chart type={"repairType"} />
+        </div>
+        <div className={styles["dashboard__stats__content__chart"]} style={{ gridColumn: "1 / 9", gridRow: "2 / 3" }}>
+          <h3>Nhóm linh kiện</h3>
+          <Chart type={"category"} />
+        </div>
+        <div className={styles["dashboard__stats__content__chart"]} style={{ gridColumn: "6 / 9", gridRow: "1 / 2", }}>
+          <h3>Doanh thu</h3>
+          <Chart type={"totalprice"} />
+        </div>
       </div>
+      <h3>Top các nhóm linh kiện</h3>
+      <TopCategories />
     </div>
   );
 }
