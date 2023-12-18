4c7f3d328f59c3b1824149295ae547604069d684
khanhndxd <khanhprox25x@gmail.com>
2023-12-18 16:27:19 +0700

    Update sidebar component layout

--- src/components/pages/main/SidebarAccount.js
+++ src/components/pages/main/SidebarAccount.js
@@ -1,21 +1,30 @@
 "use client";
 import styles from "@/styles/main.module.scss";
-import { useDispatch } from "react-redux";
-import { showNotification } from "@/store/features/notificationSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { logOut } from "@/store/features/authSlice";
-import { hideLoading, showLoading } from "@/store/features/loadingAsyncSlice";
+import { showLoading } from "@/store/features/loadingAsyncSlice";
+import { useEffect, useState } from "react";
 
 export default function SidebarAccount() {
+  const [user, setUser] = useState({ name: "", role: "" });
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
 
   const handleLogout = () => {
     dispatch(showLoading({ content: "Đang đăng xuất khỏi tài khoản..." }));
     dispatch(logOut());
   };
 
+  useEffect(() => {
+    if (auth) {
+      setUser({ name: auth.user, role: auth.role });
+    }
+  }, [auth]);
+
   return (
     <div className={styles["sidebar__account"]}>
-      <button onClick={handleLogout} className={styles["button"]}>
+      <h6>{user.name}</h6>
+      <button onClick={handleLogout} className={styles["button"]} style={{ marginTop: "10px" }}>
         Đăng xuất
       </button>
     </div>
